ff07c019535b8bb1c93631aabf1a9c122769cdca
Chris Paxton <cpaxton@fb.com>
2022-09-16 11:55:25 -0500

    update grasping server

--- contact_graspnet/graspnet_ros_server.py
+++ contact_graspnet/graspnet_ros_server.py
@@ -59,42 +59,29 @@ def inference(global_config, checkpoint_dir, input_paths, K=None, local_regions=
 
     server = GraspServer()
 
-    # Process example test scenes
-    for p in glob.glob(input_paths):
-        print('Loading ', p)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
 
-        pc_segments = {}
-        segmap, rgb, depth, cam_K, pc_full, pc_colors = load_available_input_data(p, K=K)
-        
-        if segmap is None and (local_regions or filter_grasps):
-            raise ValueError('Need segmentation map to extract local regions or filter grasps')
+        # GET INPUTS
+        has_input = False
+        if not has_input:
+            rate.sleep()
+            continue
 
-        if pc_full is None:
-            print('Converting depth to point cloud(s)...')
-            pc_full, pc_segments, pc_colors = grasp_estimator.extract_point_clouds(depth, cam_K, segmap=segmap, rgb=rgb,
-                                                                                    skip_border_objects=skip_border_objects, z_range=z_range)
-        else:
-            pc_segmap = segmap.reshape(-1)
-            for i in np.unique(pc_segmap):
-                if i == 0: continue
-                else:
-                    pc_segments[i] = pc_full[pc_segmap == i]
+        pc_segmap = segmap.reshape(-1)
+        for i in np.unique(pc_segmap):
+            if i == 0: continue
+            else:
+                pc_segments[i] = pc_full[pc_segmap == i]
 
-        print('Generating Grasps...')
-        pred_grasps_cam, scores, contact_pts, _ = grasp_estimator.predict_scene_grasps(sess, pc_full, pc_segments=pc_segments, 
-                                                                                          local_regions=local_regions, filter_grasps=filter_grasps, forward_passes=forward_passes)  
-
-        # Save results
-        np.savez('results/predictions_{}'.format(os.path.basename(p.replace('png','npz').replace('npy','npz'))), 
-                  pred_grasps_cam=pred_grasps_cam, scores=scores, contact_pts=contact_pts)
-
-        # Visualize results          
-        print(rgb.shape)
+        pred_grasps_cam, scores, contact_pts, _ = grasp_estimator.predict_scene_grasps(sess, pc_full,
+                                                                                       pc_segments=pc_segments, 
+                                                                                       local_regions=local_regions,
+                                                                                       filter_grasps=filter_grasps,
+                                                                                       forward_passes=forward_passes)  
         show_image(rgb, segmap)
         visualize_grasps(pc_full, pred_grasps_cam, scores, plot_opencv_cam=True, pc_colors=pc_colors)
-        
-    if not glob.glob(input_paths):
-        print('No files found: ', input_paths)
+
         
 if __name__ == "__main__":
 
