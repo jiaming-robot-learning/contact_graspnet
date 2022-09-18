3a34dcf5d4ba0e8d4fb4b55e35badd2e1b9f1d1d
Chris Paxton <cpaxton@fb.com>
2022-09-18 16:36:16 -0500

    revert size - this was not the issue

--- contact_graspnet/test_server.py
+++ contact_graspnet/test_server.py
@@ -3,7 +3,8 @@ from home_robot.ros.grasp_helper import GraspClient
 from data import load_available_input_data
 
 rospy.init_node('test_grasping')
-p = "stretch_2022_09_14-12_51_59.npy"
+#p = "stretch_2022_09_14-12_51_59.npy"
+p = "stretch_2022_09_18-16_21_37.npy"
 segmap, rgb, depth, cam_K, pc_full, pc_colors = load_available_input_data(p, K=None)
 
 # TODO remove debug code
